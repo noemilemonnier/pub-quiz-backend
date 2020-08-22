fff2d8ff674be727492214b16cf1bbbb3a3b1761
Noemi Lemonnier <35566817+noemilemonnier@users.noreply.github.com>
2020-08-22 19:01:24 +0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //url: "mongodb://localhost:27017/noemilemonnier_db"
-    url: "mongodb+srv://nlemonn:ABC123@cluster0.datqc.azure.mongodb.net/pub-quiz"
+    url: "mongodb+srv://nlemonn:*****@cluster0.datqc.azure.mongodb.net/pub-quiz"
   };
 
   
